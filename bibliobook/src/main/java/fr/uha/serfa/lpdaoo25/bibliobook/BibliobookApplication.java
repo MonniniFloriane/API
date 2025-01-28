0a24b08ed059bf372dcf2c1782eea69e5cacf64c
dev_flo <florianemonnini@gmail.com>
2025-01-28 14:08:10 +0100

    first commit

---
+++ bibliobook/src/main/java/fr/uha/serfa/lpdaoo25/bibliobook/BibliobookApplication.java
@@ -0,0 +1,13 @@
+package fr.uha.serfa.lpdaoo25.bibliobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliobookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliobookApplication.class, args);
+	}
+
+}
---
+++ bibliobook/src/test/java/fr/uha/serfa/lpdaoo25/bibliobook/BibliobookApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.uha.serfa.lpdaoo25.bibliobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
